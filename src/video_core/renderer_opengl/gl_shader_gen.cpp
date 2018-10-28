d67e36cd7155d3614853e073814342a492cf1814
FernandoS27 <fsahmkow27@gmail.com>
2018-10-27 21:57:56 -0400

    Set GL_ARB_gpu_shader5 as a requirement

--- src/video_core/renderer_opengl/gl_shader_gen.cpp
+++ src/video_core/renderer_opengl/gl_shader_gen.cpp
@@ -16,7 +16,7 @@ static constexpr u32 PROGRAM_OFFSET{10};
 ProgramResult GenerateVertexShader(const ShaderSetup& setup) {
     std::string out = "#version 430 core\n";
     out += "#extension GL_ARB_separate_shader_objects : enable\n";
-    out += "#extension GL_ARB_gpu_shader5 : enable\n\n";
+    out += "#extension GL_ARB_gpu_shader5 : require\n\n";
     out += Decompiler::GetCommonDeclarations();
 
     out += R"(
@@ -85,7 +85,7 @@ void main() {
 ProgramResult GenerateGeometryShader(const ShaderSetup& setup) {
     std::string out = "#version 430 core\n";
     out += "#extension GL_ARB_separate_shader_objects : enable\n";
-    out += "#extension GL_ARB_gpu_shader5 : enable\n\n";
+    out += "#extension GL_ARB_gpu_shader5 : require\n\n";
     out += Decompiler::GetCommonDeclarations();
     out += "bool exec_geometry();\n";
 
@@ -120,7 +120,7 @@ void main() {
 ProgramResult GenerateFragmentShader(const ShaderSetup& setup) {
     std::string out = "#version 430 core\n";
     out += "#extension GL_ARB_separate_shader_objects : enable\n";
-    out += "#extension GL_ARB_gpu_shader5 : enable\n\n";
+    out += "#extension GL_ARB_gpu_shader5 : require\n\n";
     out += Decompiler::GetCommonDeclarations();
     out += "bool exec_fragment();\n";
 
